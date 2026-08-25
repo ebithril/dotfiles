fd23559be0eb60ae35b02ffe637891f02b0c4ee3
Patrik Görling <p.gorling@gmail.com>
2026-08-25 06:19:12 +0200

    chore: reset config

--- hypr/scripts/hyprpaperStart.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-WALLPAPER_DIR="$HOME/dotfiles/assets/wallpapers/"
-
-WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
-
-hyprctl hyprpaper wallpaper ,"$WALLPAPER"
--- quickshell/bar/components/ClockWidget.qml
+++
@@ -1,8 +0,0 @@
-import QtQuick
-
-Text {
-    text: Time.time
-    color: Theme.foregroundColor
-    font.pixelSize: 12
-    font.family: Theme.textFont
-}
